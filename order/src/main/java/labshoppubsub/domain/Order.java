876fc1eeef4c7457af394597925c2d5963273f29
Shin bo-cheol <doori72@gmail.com>
2022-11-26 15:27:05 +0000

    commit

--- order/src/main/java/labshoppubsub/domain/Order.java
+++ order/src/main/java/labshoppubsub/domain/Order.java
@@ -75,21 +75,21 @@ public class Order  {
     public static void updateStatus(DeliveryStarted deliveryStarted){
 
         /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
+         Order order = new Order();
+         repository().save(order);
+         */
+         
 
         /** Example 2:  finding and process
+         */
         
-        repository().findById(deliveryStarted.get???()).ifPresent(order->{
+        repository().findById(deliveryStarted.getOrderId()).ifPresent(order->{
             
-            order // do something
+            // order // do something
             repository().save(order);
 
 
          });
-        */
 
         
     }
